18c8fc212770c281e4e44d08b36cfd13aca5f514
Rashedul Islam <rashedulraha.bd@gmail.com>
2026-03-02 05:53:02 +0600

    practice typescript

--- b-practice-typescript/Src/index.ts
+++ b-practice-typescript/Src/index.ts
@@ -22,9 +22,51 @@ import type ___type_script = require("./___type_script");
 
 // ? Square bracket syntax
 
-let numbers: number[] = [2, 2, 41, 8];
-let string: string[] = ["Rashedul islam"];
+// let numbers: number[] = [2, 2, 41, 8];
+// let string: string[] = ["Rashedul islam"];
 
-//? Generic syntax
-let number: Array<number> = [23, 23, 522];
-let flags: Array<boolean> = [true, false];
+// ? Generic syntax
+// let number: Array<number> = [23, 23, 522];
+// let flags: Array<boolean> = [true, false];
+
+// !tuple
+
+// type stringNumberPair = [string, number];
+
+// const pair: stringNumberPair = ["hello", 45];
+
+// const user = {
+//   name: "Rashed",
+//   age: 20,
+// };
+
+// const user: { name: string; age: number } = {
+//   name: "Rashed",
+//   age: 24,
+// };
+
+// interface User {
+//   name: string;
+//   age: number;
+// }
+
+// const u: User = {
+//   name: "Rayhan",
+//   age: 21
+// };
+
+//! type compatibility
+
+//? 1 Combining
+
+// union type
+
+//  type operator
+
+// type typeAB = typeA & typeA
+// type typeAB = typeAB & typeAB;
+
+//!  intersection
+
+type a = { name: string };
+type b = { age: number };
