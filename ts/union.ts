3ff4e41fc8470ced2b66a9421a537e7193703dc3
Rashedul Islam <rashedulraha.bd@gmail.com>
2026-03-02 14:48:56 +0600

    type union

---
+++ ts/union.ts
@@ -0,0 +1,40 @@
+{
+  // union types
+
+  // type FrontendDeveloper = "FakibazDeveloper" | "JuniorDeveloper";
+  // type FullStackDeveloper = "FrontendDeveloper" | "ExpertDeveloper";
+  // type Developer = FrontendDeveloper | FullStackDeveloper;
+
+  // union and other
+
+  // type User = {
+  //   name: string;
+  //   email?: string;
+  //   gender: "male" | "female";
+  //   bloodGroup: "A+" | "B+" | "AB+" | "O+";
+  // };
+
+  //   use user type alias
+
+  // const user1: User = {
+  //   name: "Rashedul",
+  //   gender: "male",
+  //   bloodGroup: "AB+",
+  // };
+
+  type FrontendDeveloper = {
+    skills: string[];
+    designation1: "FrontendDeveloper";
+  };
+  type BackendDeveloper = {
+    skills: string[];
+    designation2: "BackendDeveloper";
+  };
+  type FullStackDeveloper = FrontendDeveloper & BackendDeveloper;
+
+  const fullStackDeveloper: FullStackDeveloper = {
+    skills: ["HTML", "CSS", "JS", "REACT"],
+    designation1: "FrontendDeveloper",
+    designation2: "BackendDeveloper",
+  };
+}
