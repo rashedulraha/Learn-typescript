523cb7233fa1d00f1c717feffb34304c9c259f72
Rashedul Islam <rashedulraha.bd@gmail.com>
2026-03-03 15:19:51 +0600

    function

--- Advance_typescript/function_with_generic.ts
+++ Advance_typescript/function_with_generic.ts
@@ -1,4 +1,20 @@
 {
   //!  function with generic
+
+  const createArray = (param:string):string[]=>{
+    return [param]
+  }
+
+  //! function with generic
   
+  const createArrayWithGeneric = <T>(param:T):T[]=>{
+
+    return [param]
+  }
+   
+  const genericResult = createArrayWithGeneric<boolean>(flase)
+
+  const  result = createArray("Bangladesh");
+  console.log(result)
+   
 }
