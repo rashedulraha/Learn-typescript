02e16d3ab5803cd7d352d5316b86214e9217378b
Rashedul Islam <rashedulraha.bd@gmail.com>
2026-03-03 06:08:03 +0600

    type assertion

---
+++ Advance_typescript/assertion.ts
@@ -0,0 +1,23 @@
+{
+  //! type assertion
+  let anything: any;
+
+  anything = "Next level web development";
+
+  anything = 202;
+  //  ( anything as number).
+
+  const kgToGm = (value: string | number): string | number | undefined => {
+    if (typeof value === "string") {
+      const convertedValue = parseFloat(value) * 1000;
+      return `The converted value is : ${convertedValue}`;
+    }
+
+    if (typeof value === "number") {
+      return value * 1000;
+    }
+  };
+
+  const result1 = kgToGm(1000) as number;
+  const result2 = kgToGm("5000") as string;
+}
