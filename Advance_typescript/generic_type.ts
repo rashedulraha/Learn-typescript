00d39c8a1b510f2ef478fb0e7a187e6964c37271
Rashedul Islam <rashedulraha.bd@gmail.com>
2026-03-03 06:58:46 +0600

    generic type

--- Advance_typescript/generic_type.ts
+++ Advance_typescript/generic_type.ts
@@ -1,7 +1,7 @@
 {
   //!  generic type
 
-  type GenericArray<params> = Array<params>;
+  type GenericArray<T> = Array<T>;
 
   // const rollNumber: number[] = [3, 5, 5, 8, 6, 8];
   const rollNumber: GenericArray<number> = [3, 5, 5, 8, 6, 8];
@@ -12,4 +12,25 @@
   // const booleanArray: boolean[] = [true, false, true];
   const booleanArray: GenericArray<boolean> = [true, false, true];
   //
+
+  //  Array of object using generic
+
+  type User = { name: string; age: number }[];
+
+  const user: GenericArray<{ name: string; age: number }> = [
+    { name: "Rashedul", age: 45 },
+    { name: "Romman", age: 45 },
+    { name: "Abdullah", age: 56 },
+  ];
+
+  //  generic tuple
+
+  type GenericTuple<X, Y> = [X, Y];
+
+  const mens: GenericTuple<string, string> = ["Mr.X", "Mr.Y"];
+
+  const personData: GenericTuple<
+    number,
+    { name: string; email: string; age: number }
+  > = [120, { name: "Rashedul", email: "emil@gmail.com", age: 45 }];
 }
