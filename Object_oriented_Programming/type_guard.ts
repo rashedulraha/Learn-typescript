dc1acb7006c6eaed2ed185c2e34e2c08ed5aac46
Rashedul Islam <rashedulraha.bd@gmail.com>
2026-03-04 14:53:09 +0600

    type guard

--- Object_oriented_Programming/type_guard.ts
+++ Object_oriented_Programming/type_guard.ts
@@ -11,4 +11,37 @@
       return param1.toString() + param2.toString();
     }
   };
+
+  const result1 = add(2, 5);
+  console.log(result1);
+  const result2 = add("5", "5");
+  console.log(result2);
+
+  //  type in guard
+
+  type NormalUser = {
+    name: string;
+  };
+
+  type AdminUser = {
+    name: string;
+    role: "Admin";
+  };
+
+  const getUser = (user: NormalUser | AdminUser) => {
+    if ("role" in user) {
+      console.log(`My name  is ${user.name} my role is ${user.role}`);
+    }
+  };
+
+  const normalUser: NormalUser = {
+    name: "Mr. Normal Bhai",
+  };
+
+  const adminUser: AdminUser = {
+    name: "Mr. Admin Bhai",
+    role: "Admin",
+  };
+
+  getUser(adminUser);
 }
