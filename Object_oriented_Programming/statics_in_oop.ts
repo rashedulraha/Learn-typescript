f7be1e39c600ef574f3c60e63bdc8cee80ff51c6
Rashedul Islam <rashedulraha.bd@gmail.com>
2026-03-04 17:11:03 +0600

    statics

--- Object_oriented_Programming/statics_in_oop.ts
+++ Object_oriented_Programming/statics_in_oop.ts
@@ -1,3 +1,20 @@
 {
-  console.log("Hello world");
+  //   statics
+
+  class Counter {
+    count: number = 0;
+    increment() {
+      return (this.count = this.count + 1);
+    }
+
+    decrement() {
+      return (this.count = this.count - 1);
+    }
+  }
+
+  const instances1 = new Counter();
+  console.log(instances1.increment());
+
+  const instances2 = new Counter();
+  console.log(instances2.increment());
 }
